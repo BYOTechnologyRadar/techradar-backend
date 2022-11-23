df792f8bbc89069af59258f809dcbbcbc2b4191a
sargarakshay <akshay.sargar4141@gmail.com>
2022-11-23 20:07:05 +0530

    [IF-184] initial commit

---
+++ src/test/java/com/app/techradarbackend/TechradarBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.techradarbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechradarBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
