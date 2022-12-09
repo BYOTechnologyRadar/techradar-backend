4aa0e3e6fc6be2381adff379eb2cff014577f136
sargarakshay <akshay.sargar4141@gmail.com>
2022-12-09 19:42:37 +0530

    [IF-184] feat: add custom exception handling

---
+++ src/main/java/com/app/techradarbackend/exception/GlobalExceptionHandler.java
@@ -0,0 +1,36 @@
+package com.app.techradarbackend.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<ApiResponse> resourceNotFoundExceptionHandler(ResourceNotFoundException ex, WebRequest webRequest) {
+        String message = ex.getMessage();
+        ApiResponse apiResponse = new ApiResponse(LocalDateTime.now(), message, HttpStatus.NOT_FOUND.getReasonPhrase(),webRequest.getDescription(false));
+        return new ResponseEntity<>(apiResponse, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
+        Map<String, String> violations = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+
+            violations.put(fieldName, errorMessage);
+        });
+
+        return new ResponseEntity<>(violations, HttpStatus.BAD_REQUEST);
+    }
+}
