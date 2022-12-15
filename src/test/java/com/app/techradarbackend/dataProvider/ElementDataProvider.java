00e2cdd4f792e0f232fe990ff394d9eb5fbb5187
Akshay Sargar <akshay.sargar4141@gmail.com>
2022-12-15 19:02:27 +0530

    [IF-184] feat(test): add element service and controller tests and data provider

---
+++ src/test/java/com/app/techradarbackend/dataProvider/ElementDataProvider.java
@@ -0,0 +1,147 @@
+package com.app.techradarbackend.dataProvider;
+
+import com.app.techradarbackend.dto.CategoryDTO;
+import com.app.techradarbackend.dto.ElementCreateDTO;
+import com.app.techradarbackend.dto.ElementDTO;
+import com.app.techradarbackend.dto.ElementUpdateDTO;
+import com.app.techradarbackend.entity.CategoryEntity;
+import com.app.techradarbackend.entity.ElementEntity;
+import com.app.techradarbackend.enums.ElementLevel;
+import com.app.techradarbackend.enums.ElementStatus;
+import com.app.techradarbackend.enums.ElementVersion;
+import org.testng.annotations.DataProvider;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ElementDataProvider {
+    @DataProvider
+    public static Object[][] getElementCreateDTOAndElementEntityAndElementDTO() {
+        final ElementCreateDTO elementCreateDTO = createElementCreateDTO();
+        final ElementEntity elementEntity = createElementEntity();
+        final ElementDTO elementDTO = createElementDTO();
+
+        return new Object[][] {{elementCreateDTO, elementEntity, elementDTO}};
+    }
+
+    @DataProvider
+    public static Object[][] getElementUpdateDTOAndElementEntityAndElementDTO() {
+        final ElementUpdateDTO elementUpdateDTO = createElementUpdateDTO();
+        final ElementEntity elementEntity = createElementEntity();
+        final ElementDTO elementDTO = createElementDTO();
+
+        return new Object[][] {{elementUpdateDTO, elementEntity, elementDTO}};
+    }
+
+    @DataProvider
+    public static Object[][] getElementEntityListAndElementDTOListAndElementEntity() {
+        final List<ElementEntity> elementEntityList = createElementEntityList();
+        final List<ElementDTO> elementDTOList = createElementDTOList();
+
+        return new Object[][] {{elementEntityList, elementDTOList}};
+    }
+
+    @DataProvider
+    public static Object[][] getElementEntityAndElementDTO() {
+        final ElementEntity elementEntity = createElementEntity();
+        final ElementDTO elementDTO = createElementDTO();
+
+        return new Object[][] {{elementEntity, elementDTO}};
+    }
+
+    private static ElementEntity createElementEntity() {
+        final ElementEntity elementEntity = new ElementEntity();
+        elementEntity.setName("Java");
+        elementEntity.setDescription("Java is a high-level, class-based, object-oriented programming language that is designed to have as few implementation dependencies as possible.");
+        elementEntity.setStatus(ElementStatus.ACTIVE);
+        elementEntity.setVersion(ElementVersion.New);
+        elementEntity.setLevel(ElementLevel.ADOPT);
+        elementEntity.setCategory(new CategoryEntity());
+
+        return elementEntity;
+    }
+
+    private static ElementDTO createElementDTO() {
+        final ElementDTO elementDTO = new ElementDTO();
+        elementDTO.setName("Java");
+        elementDTO.setDescription("Java is a high-level, class-based, object-oriented programming language that is designed to have as few implementation dependencies as possible.");
+        elementDTO.setStatus(ElementStatus.ACTIVE);
+        elementDTO.setVersion(ElementVersion.New);
+        elementDTO.setLevel(ElementLevel.ADOPT);
+        elementDTO.setCategory(new CategoryDTO());
+
+        return elementDTO;
+    }
+
+    private static ElementCreateDTO createElementCreateDTO() {
+        final ElementCreateDTO elementCreateDTO = new ElementCreateDTO();
+        elementCreateDTO.setName("Java");
+        elementCreateDTO.setDescription("Java is a high-level, class-based, object-oriented programming language that is designed to have as few implementation dependencies as possible.");
+        elementCreateDTO.setLevel(ElementLevel.ADOPT);
+        elementCreateDTO.setStatus(ElementStatus.ACTIVE);
+        elementCreateDTO.setVersion(ElementVersion.Unchanged);
+        elementCreateDTO.setCategoryId(1);
+
+        return elementCreateDTO;
+    }
+
+    private static ElementUpdateDTO createElementUpdateDTO() {
+        final ElementUpdateDTO elementUpdateDTO = new ElementUpdateDTO();
+        elementUpdateDTO.setName("Java");
+        elementUpdateDTO.setDescription("Java is a high-level, class-based, object-oriented programming language that is designed to have as few implementation dependencies as possible.");
+        elementUpdateDTO.setLevel(ElementLevel.ADOPT);
+        elementUpdateDTO.setStatus(ElementStatus.ACTIVE);
+        elementUpdateDTO.setVersion(ElementVersion.Unchanged);
+
+        return elementUpdateDTO;
+    }
+
+    private static List<ElementEntity> createElementEntityList() {
+        List<ElementEntity> elementEntityList = new ArrayList<>();
+        ElementEntity elementEntity1 = new ElementEntity();
+        elementEntity1.setName("Java");
+        elementEntity1.setDescription("Java is a high-level, class-based, object-oriented programming language that is designed to have as few implementation dependencies as possible.");
+        elementEntity1.setStatus(ElementStatus.ACTIVE);
+        elementEntity1.setVersion(ElementVersion.Changed);
+        elementEntity1.setLevel(ElementLevel.ADOPT);
+        elementEntity1.setCategory(new CategoryEntity());
+        elementEntityList.add(elementEntity1);
+
+        ElementEntity elementEntity2 = new ElementEntity();
+        elementEntity2.setName("Spring Boot");
+        elementEntity2.setDescription("Spring Boot is an open source Java-based framework used to create a micro Service. It is developed by Pivotal Team and is used to build stand-alone and production ready spring applications.");
+        elementEntity2.setStatus(ElementStatus.ACTIVE);
+        elementEntity2.setVersion(ElementVersion.Unchanged);
+        elementEntity2.setLevel(ElementLevel.TRIAL);
+        elementEntity2.setCategory(new CategoryEntity());
+        elementEntityList.add(elementEntity2);
+
+        return elementEntityList;
+    }
+
+    private static List<ElementDTO> createElementDTOList() {
+        final List<ElementDTO> elementDTOList = new ArrayList<>();
+        ElementDTO elementDTO1 = new ElementDTO();
+        elementDTO1.setName("Java");
+        elementDTO1.setDescription("Java is a high-level, class-based, object-oriented programming language that is designed to have as few implementation dependencies as possible.");
+        elementDTO1.setStatus(ElementStatus.ACTIVE);
+        elementDTO1.setVersion(ElementVersion.Changed);
+        elementDTO1.setLevel(ElementLevel.ADOPT);
+        elementDTO1.setCategory(new CategoryDTO());
+        elementDTOList.add(elementDTO1);
+
+        ElementDTO elementDTO2 = new ElementDTO();
+        elementDTO2.setName("Spring Boot");
+        elementDTO2.setDescription("Spring Boot is an open source Java-based framework used to create a micro Service. It is developed by Pivotal Team and is used to build stand-alone and production ready spring applications.");
+        elementDTO2.setStatus(ElementStatus.ACTIVE);
+        elementDTO2.setVersion(ElementVersion.Unchanged);
+        elementDTO2.setLevel(ElementLevel.TRIAL);
+        elementDTO2.setCategory(new CategoryDTO());
+        elementDTOList.add(elementDTO2);
+
+        return elementDTOList;
+    }
+
+
+
+}
---
+++ src/test/java/com/app/techradarbackend/service/ElementServiceTests.java
@@ -0,0 +1,88 @@
+package com.app.techradarbackend.service;
+
+import com.app.techradarbackend.dao.ElementDAO;
+import com.app.techradarbackend.dataProvider.ElementDataProvider;
+import com.app.techradarbackend.dto.ElementCreateDTO;
+import com.app.techradarbackend.dto.ElementDTO;
+import com.app.techradarbackend.dto.ElementUpdateDTO;
+import com.app.techradarbackend.entity.ElementEntity;
+import com.app.techradarbackend.mapper.ElementMapper;
+import com.app.techradarbackend.mapper.ElementMapperImpl;
+import com.app.techradarbackend.service.impl.ElementServiceImpl;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.Spy;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+public class ElementServiceTests {
+    @Mock
+    private ElementDAO elementDAO;
+
+    @Mock
+    private CategoryService categoryService;
+
+    @InjectMocks
+    private ElementService elementService;
+
+    @Spy
+    private ElementMapper elementMapper = new ElementMapperImpl();
+
+    @BeforeClass
+    public void initMock() {
+        elementService = new ElementServiceImpl();
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test(dataProvider = "getElementCreateDTOAndElementEntityAndElementDTO", dataProviderClass = ElementDataProvider.class)
+    public void ES_01_Add_Element_Successful(final ElementCreateDTO elementCreateDTO, final ElementEntity elementEntity, ElementDTO elementDTO) {
+        when(elementMapper.mapCreateDTOToEntity(elementCreateDTO)).thenReturn(elementEntity);
+        when(elementDAO.save(elementEntity)).thenReturn(elementEntity);
+        when(elementService.addElement(elementCreateDTO)).thenReturn(elementDTO);
+        when(elementMapper.mapEntityToDTO(elementEntity)).thenReturn(elementDTO);
+
+        final ElementDTO element = elementService.addElement(elementCreateDTO);
+        Assert.assertNotNull(element);
+    }
+
+    @Test(dataProvider = "getElementUpdateDTOAndElementEntityAndElementDTO", dataProviderClass = ElementDataProvider.class)
+    public void ES_02_Update_Element_Successful(final ElementUpdateDTO elementUpdateDTO, final ElementEntity elementEntity, final ElementDTO elementDTO) {
+        when(elementDAO.findById(1)).thenReturn(Optional.of(elementEntity));
+        when(elementService.updateElement(1, elementUpdateDTO)).thenReturn(elementDTO);
+
+        verify(elementMapper, times(1)).mapUpdateDTOToEntity(elementUpdateDTO, elementEntity);
+    }
+
+    @Test(dataProvider = "getElementEntityListAndElementDTOListAndElementEntity", dataProviderClass = ElementDataProvider.class)
+    public void ES_03_Get_All_Elements_Successful(List<ElementEntity> elementEntityList, List<ElementDTO> elementDTOList) {
+        when(elementDAO.findAll()).thenReturn(elementEntityList);
+        when(elementService.getAllElements()).thenReturn(elementDTOList);
+
+        verify(elementDAO, times(1)).findAll();
+    }
+
+    @Test(dataProvider = "getElementEntityAndElementDTO", dataProviderClass = ElementDataProvider.class)
+    public void ES_04_Get_Element_Successful(ElementEntity elementEntity, ElementDTO elementDTO) {
+        when(elementDAO.findById(1)).thenReturn(Optional.of(elementEntity));
+        when(elementService.getElementByElementId(1)).thenReturn(elementDTO);
+
+        final ElementDTO element = elementService.getElementByElementId(1);
+        Assert.assertNotNull(element);
+    }
+
+    @Test
+    public void ES_05_Delete_Element_Successful() {
+        elementService.deleteElementByElementId(1);
+
+        verify(elementDAO, times(1)).deleteById(1);
+    }
+}
