6cd284d6223af91d96d68ee9f604bb51e7c7fd6a
Akshay Sargar <akshay.sargar4141@gmail.com>
2022-12-20 21:08:36 +0530

    [IF-184] refactor(dao): add get categories by radar method to DAO

--- src/main/java/com/app/techradarbackend/dao/CategoryDAO.java
+++ src/main/java/com/app/techradarbackend/dao/CategoryDAO.java
@@ -1,10 +1,14 @@
 package com.app.techradarbackend.dao;
 
 import com.app.techradarbackend.entity.CategoryEntity;
+import com.app.techradarbackend.entity.RadarEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CategoryDAO extends JpaRepository<CategoryEntity, Integer>, QuerydslPredicateExecutor<CategoryEntity> {
+    List<CategoryEntity> getCategoryEntitiesByRadar(RadarEntity radar);
 }
